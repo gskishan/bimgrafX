8a05b111e44acc192bac993be99df234781bcfe4
bhanuerp <bhanu.g@bimgrafx.org>
2026-03-24 22:04:24 +0530

    Update employee_boarding_controller.py

--- bimgrafx/overrides/employee_boarding_controller.py
+++ bimgrafx/overrides/employee_boarding_controller.py
@@ -4,31 +4,36 @@ from hrms.controllers.employee_boarding_controller import EmployeeBoardingContro
 
 class CustomEmployeeBoardingController(EmployeeBoardingController):
     def on_submit(self):
-        # Ensure Project Code is set
-        if not self.project_code:
-            frappe.throw(_("Project Code is required"))
-
-        # Create the project for the given employee onboarding
-        project_name = _(self.doctype) + " : "
-        if self.doctype == "Employee Onboarding":
-            project_name += self.job_applicant
-        else:
-            project_name += self.employee
-
-        project = frappe.get_doc(
-            {
-                "doctype": "Project",
-                "project_name": project_name,
-                "expected_start_date": self.date_of_joining
-                if self.doctype == "Employee Onboarding"
-                else self.resignation_letter_date,
-                "department": self.department,
-                "company": self.company,
-                "project_code": self.project_code,  # Ensure Project Code is passed here
-            }
-        ).insert(ignore_permissions=True, ignore_mandatory=True)
-
-        self.db_set("project", project.name)
-        self.db_set("boarding_status", "Pending")
-        self.reload()
-        self.create_task_and_notify_user()
+	    # Ensure Project field is set
+	    if not self.project:
+	        # Generate a default Project Name if not provided
+	        project_name = _(self.doctype) + " : "
+	        if self.doctype == "Employee Onboarding":
+	            project_name += self.job_applicant
+	        else:
+	            project_name += self.employee
+	
+	        project = frappe.get_doc(
+	            {
+	                "doctype": "Project",
+	                "project_name": project_name,
+	                "expected_start_date": self.date_of_joining
+	                if self.doctype == "Employee Onboarding"
+	                else self.resignation_letter_date,
+	                "department": self.department,
+	                "company": self.company,
+	            }
+	        ).insert(ignore_permissions=True, ignore_mandatory=True)
+	
+	        # Set the generated project name in the Employee Onboarding document
+	        self.db_set("project", project.name)
+	
+	    # Ensure the linked Project has a Project Code
+	    project = frappe.get_doc("Project", self.project)
+	    if not project.project_code:
+	        frappe.throw(_("The linked Project must have a Project Code."))
+	
+	    # Update the boarding status
+	    self.db_set("boarding_status", "Pending")
+	    self.reload()
+	    self.create_task_and_notify_user()
