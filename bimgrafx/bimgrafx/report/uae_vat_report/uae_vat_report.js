bf9483e80ff5c0fdf147d6e50f7744acfc8fc96b
bhanuerp <bhanu.g@bimgrafx.org>
2026-04-13 14:13:51 +0530

    Create uae_vat_report.js

---
+++ bimgrafx/bimgrafx/report/uae_vat_report/uae_vat_report.js
@@ -0,0 +1,29 @@
+frappe.query_reports["UAE VAT 201"] = {
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+			on_change: function () {
+				frappe.query_report.refresh();
+			},
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+		},
+	],
+};
